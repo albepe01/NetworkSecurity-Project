7ce019779052344912f7a2ec0dec6114fceaa0fc
albepe01 <128582852+albepe01@users.noreply.github.com>
2025-02-16 15:50:43 +0100

    Add files via upload

--- apache_reverse_proxy/server.py
+++ apache_reverse_proxy/server.py
@@ -2,14 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/test', methods=['GET','POST'])
+@app.route('/test', methods=['POST'])
 def test():
-    return "Test OK", 200
+    return "Test OK\n", 200
 
-@app.route('/hidden_test', methods=['GET','POST'])
+@app.route('/hidden_test', methods=['POST'])
 def hidden_test():
-    return "Hidden Test OK", 200
+    return "Hidden Test OK\n", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
